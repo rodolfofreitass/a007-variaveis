bdac1b549269c6f6b59c39b9cc152f13d8521f3f
rodolfofreitass <rodolfofreitas38@hotmail.com>
2022-07-19 20:07:16 -0300

    Atividade A007 resolvida

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let nome=prompt("Qual seu nome?")
+console.log(nome)
+let idade= prompt("Qual sua idade?")
+console.log(idade)
+
+// O tipo das variaveis foram string porque sempre que for inserido pelo prompt, ele interpreta como um texto, logo variavel do tipo string.
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log("Olá",nome ,"você tem",idade," anos")
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const perguntaum = "Você está usando roupa preta?"
+const respostaum = prompt(perguntaum)
+const perguntadois = "Você está usando roupa vermelha?"
+const respostadois = prompt(perguntadois)
+const perguntatres = "Você está usando roupa branca?"
+const respostatres = prompt(perguntatres)
+
+console.log(perguntaum, respostaum)
+console.log(perguntadois, respostadois)
+console.log(perguntatres, respostatres)
